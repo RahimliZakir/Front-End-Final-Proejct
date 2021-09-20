c8dc6c119614a942511438809f1141674058cf7d
RahimliZakir <zakirer@code.edu.az>
2021-09-20 22:09:39 +0400

    Navbar Color Change On Scroll Event With Chaining

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,6 +2,16 @@ $(document).ready(function () {
   //! jQuery HTML Partial
   $("header").load("_header.html");
 
+  $(window).on("scroll", function () {
+    let scrollTop = $(window).scrollTop();
+
+    if (scrollTop > 0) {
+      $("nav").css("background-color", "#000000").css("transition", "0.5s");
+    } else {
+      $("nav").css("background-color", "transparent").css("transition", "0.5s");
+    }
+  });
+
   $("#brand-select").niceSelect();
   $("#model-select").niceSelect();
 
