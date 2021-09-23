716fa0d167f556bde10bd3f9a1aa340802f20fba
RahimliZakir <zakirer@code.edu.az>
2021-09-23 09:24:51 +0400

    Beginning To Detail Page

--- assets/js/explore.js
+++ assets/js/explore.js
@@ -15,15 +15,6 @@ $(function () {
     }
   });
 
-  $("#brand-select").niceSelect();
-  $("#model-select").niceSelect();
-
-  let searchingForm = $(".searching-form");
-
-  $(searchingForm).submit((e) => {
-    e.preventDefault();
-  });
-
   setTimeout(() => {
     $(document.body).removeClass("loading");
     $(".loader").removeClass("active");
