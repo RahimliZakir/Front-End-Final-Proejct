2c13f5b2b8b0ed7d2955f900fbcadec51c21e61e
RahimliZakir <zakirer@code.edu.az>
2021-09-23 05:59:04 +0400

    Dynamic Car Datas From API Done

--- assets/js/explore.js
+++ assets/js/explore.js
@@ -47,4 +47,66 @@ $(function () {
   $(homeInventoryForm).submit((e) => {
     e.preventDefault();
   });
+
+  let carAPILink = "https://json-fake-api.herokuapp.com/cars";
+
+  async function getCarDatas(url) {
+    let fecthData = await fetch(url);
+    let jsonFetchData = await fecthData.json();
+
+    let fetchCol = document.querySelector(".fetch-datas-col");
+
+    for (let data of jsonFetchData) {
+      let fetchCard = document.createElement("div");
+      fetchCard.className = "fetch-card";
+      fetchCard.setAttribute("data-card-id", data.id);
+
+      let cardBody = `<div class="row fetch-datas-col-row">
+      <div class="col-5 left-fetch-data">
+        <div class="img-div">
+          <img
+            src="assets/api/${data.img_src}"
+            alt="Fetch Product"
+          />
+        </div>
+      </div>
+      <div class="col-7 right-fetch-data">
+        <h6 class="right-fetch-car-name">${data.marka}</h6>
+        <div class="fetch-model-new">
+          <h5 class="right-fetch-car-model">${data.model}</h5>
+          <p class="right-fetch-car-type">${data.new ? "(New)" : "(Used)"}</p>
+        </div>
+        <div class="fetch-car-details">
+          <div class="fetch-engine">
+            <h6>Engine</h6>
+            <h6>${data.engine}</h6>
+          </div>
+          <div class="fetch-fuel">
+            <h6>Type of fuel</h6>
+            <h6>${data.fuel}</h6>
+          </div>
+          <div class="fetch-transmission">
+            <h6>Transmission</h6>
+            <h6>${data.transmission}</h6>
+          </div>
+        </div>
+        <div class="fetch-price-and-details-btn">
+          <h3 class="right-fetch-car-price">₼${data.price}</h3>
+          <a class="right-fetch-car-details-btn" href="#"
+            >Details</a
+          >
+        </div>
+      </div>
+    </div>`;
+
+      fetchCard.innerHTML = cardBody;
+
+      fetchCol.append(fetchCard);
+
+      let fetchCardCount = document.querySelector(".fetch-card-count>span");
+      fetchCardCount.textContent = jsonFetchData.length;
+    }
+  }
+
+  getCarDatas(carAPILink);
 });
