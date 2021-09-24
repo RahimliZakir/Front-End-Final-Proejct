3ea5eda2c454cec42f95d24d17b422f715367dff
RahimliZakir <zakirer@code.edu.az>
2021-09-24 11:46:49 +0400

    Credit Simulation Page Almost Done

--- assets/js/credit-simulation.js
+++ assets/js/credit-simulation.js
@@ -1,1 +1,26 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  //! jQuery HTML Partial
+  $("nav.onscroll").load("_header.html");
+  $(".loader").load("_loader.html");
+  $("footer").load("_footer.html");
+  //! jQuery HTML Partial
+
+  $(window).on("scroll", function () {
+    let scrollTop = $(window).scrollTop();
+
+    if (scrollTop > 0) {
+      $("nav").addClass("onscroll");
+    } else {
+      $("nav").removeClass("onscroll");
+    }
+  });
+
+  setTimeout(() => {
+    $(document.body).removeClass("loading");
+    $(".loader").removeClass("active");
+    setTimeout(() => {
+      $(".loader").addClass("stop");
+    }, 2000);
+    $("nav").removeClass("pending");
+  }, 5000);
+});
